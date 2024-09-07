41d250dcba6ce0802b373f0dbe8b87f9560232d3
Sanjay-Sub1 <sanjaysubramanian@usf.edu>
2024-09-07 17:00:10 -0400

    Create determinePerformanceRating function

--- cc_3.js
+++ cc_3.js
@@ -24,6 +24,21 @@ return averageSales;
 
 }
 
+function determinePerformanceRating(salesPerEmployee){
+    if (salesPerEmployee > 10000)
+    {return "Excellent";}
+    else if (salesPerEmployee > 7000 && salesPerEmployee <= 10000)
+    {return "Good";}
+    else if (salesPerEmployee > 4000 && salesPerEmployee <= 7000)
+    {return "Satisfactory";}
+    else 
+    {return "Needs Improvement";}
+
+}
+
+
 salesFigures = [100,200,300];
 console.log(calculateAverageSales(salesFigures));
 
+averageSalesAmount = calculateAverageSales(salesFigures);
+console.log(determinePerformanceRating(averageSalesAmount));
