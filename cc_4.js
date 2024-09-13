10eb116755ae789bde8a61fdd26aaacaac0b98bf
Sanjay-Sub1 <sanjaysubramanian@usf.edu>
2024-09-13 14:17:03 -0400

    Create displayProductDetails function

--- cc_4.js
+++ cc_4.js
@@ -11,10 +11,40 @@
 </body>
 </html>
 
+//Creates an array for the inventory of products in the store, including the price, quantity, and how many items are needed for the product to not be at low stock
 const inventory = [
-    {Name : "GPU",price : 450, quantity : 20, lowStockLevel : 5},
-    {Name : "CPU", price : 250, quantity : 25, lowStockLevel : 5},
-    {Name : "Keyboard", price : 70, quantity : 30, lowStockLevel : 10},
-    {Name : "Mouse", price : 25, quantity : 75, lowStockLevel : 15},
-    {Name : "Monitor", price : 100, quantity : 20, lowStockLevel : 2}
+    {name : "GPU",price : 450, quantity : 0, lowStockLevel : 5},
+    {name : "CPU", price : 250, quantity : 25, lowStockLevel : 5},
+    {name : "Keyboard", price : 70, quantity : 30, lowStockLevel : 10},
+    {name : "Mouse", price : 25, quantity : 75, lowStockLevel : 15},
+    {name : "Monitor", price : 100, quantity : 2, lowStockLevel : 2}
 ];
+
+function displayProductDetails(products){
+    let stockStatus;
+//updates stock status to in stock if the quantity is more than the low stock level
+    if (products.quantity > products.lowStockLevel){
+        stockStatus = "In Stock";
+    }
+//updates stock status to out of stock if the quantity is 0 as a safeguard
+    else if (products.quantity == 0){
+        stockStatus = 'Out of Stock;'
+    }
+//updates the stock status to low stock if the quantity is below or equal to the low stock level
+    else {
+        stockStatus = "Low Stock";
+    }
+//logs the name, the price, the quantity and the stock status of the product
+    console.log(`Name: ${products.name}`);
+    console.log(`Price: ${products.price}$`);
+    console.log(`Quantity: ${products.quantity}`);
+    console.log(`Stock Status: ${stockStatus}`);
+}
+//creates the variable GPU to pull from the array
+GPU = inventory[0];
+//creates the variable monitor to pull from the array
+monitor = inventory[4];
+//Runs the function for the variable GPU
+displayProductDetails(GPU);
+//runs the function for the variable monitor
+displayProductDetails(monitor);
