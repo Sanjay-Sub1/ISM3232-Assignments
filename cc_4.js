cdd31ff2dd07c92eab5077b5392986798b7512ff
Sanjay-Sub1 <sanjaysubramanian@usf.edu>
2024-09-13 13:45:19 -0400

    Initialize inventory with product objects

---
+++ cc_4.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JavaScript Basics Challenge</title>
+</head>
+<body>
+    <h1>JavaScript Basics Coding Challenge</h1>
+    <script src="cc_4.js"></script>
+</body>
+</html>
+
+const inventory = [
+    {Name : "GPU",price : 450, quantity : 20, lowStockLevel : 5},
+    {Name : "CPU", price : 250, quantity : 25, lowStockLevel : 5},
+    {Name : "Keyboard", price : 70, quantity : 30, lowStockLevel : 10},
+    {Name : "Mouse", price : 25, quantity : 75, lowStockLevel : 15},
+    {Name : "Monitor", price : 100, quantity : 20, lowStockLevel : 2}
+];
